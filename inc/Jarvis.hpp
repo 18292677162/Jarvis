0ad6aae5d8b2b9aed3cb0e0d892d0351852276cf
18292677162 <2254095453@qq.com>
2019-07-03 22:35:32 +0800

    -

--- inc/Jarvis.hpp
+++ inc/Jarvis.hpp
@@ -16,16 +16,99 @@
 #include "base/http.h"
 #include "curl/curl.h"
 
-//#define SPEECH_FILE "temp_file/demo.wav"
-//#define PLAY_FILE "temp_file/play.mp3"
-//#define CMD_ETC "command.etc"
+#define SPEECH_FILE "../temp_file/demo.wav"
+#define PLAY_FILE "../temp_file/play.mp3"
+#define CMD_ETC "../command.etc"
 
 // 语音识别类
 class SpeechRec
 {
 public:
-    SpeechRec();
-    ~SpeechRec();
+    SpeechRec()
+    {
+        client = new aip::Speech(app_id, api_key, secret_key);
+    }
+
+    /**
+    * ASR语音识别示例
+    */
+    void ASR(int &err_code, std::string &message) {
+        std::cout << std::endl << "正在识别, 请稍后...(〃'▽'〃)" << std::endl;
+        
+        std::map<std::string, std::string> options;
+        // 中文显示
+        options["dev_pid"] = "1536";
+        //options["lan"] = "ZH"; 定义dev_pid会被覆盖，已废弃
+
+        // 获取文件
+        std::string file_content;
+        aip::get_file_content(SPEECH_FILE, &file_content);
+        // 频率16k wav格式
+        Json::Value result = client->recognize(file_content, "wav", 16000, options);
+        // 成功返回
+        /*
+        {
+            "err_no": 0,
+            "err_msg": "success.",
+            "corpus_no": "15984125203285346378",
+            "sn": "481D633F-73BA-726F-49EF-8659ACCC2F3D",
+            "result": ["北京天气"]
+        }
+
+        // 失败返回
+        {
+            "err_no": 2000,
+            "err_msg": "data empty.",
+            "sn": null
+        }
+        */
+        err_code = result["err_no"].asInt();
+        if (0 == err_code) {
+            message = result["result"][0].asString();
+        } else {
+            message = "识别失败...(╥╯^╰╥)";
+        }
+
+        std::cout << "语音识别本地文件结果:" << std::endl << result.toStyledString();
+    }
+
+    /**
+    * TTS语音合成示例
+    */
+    /*
+    void TTS(aip::Speech* client) {
+        std::ofstream ofile;
+        std::string file_ret;
+        std::map<std::string, std::string> options;
+        options["spd"] = "5";
+        options["per"] = "2";
+        ofile.open("./tts.mp3", std::ios::out | std::ios::binary);
+        Json::Value result = client->text2audio("百度语音合成测试", options, file_ret);
+        // 如果file_ret为不为空则说明合成成功，返回mp3文件内容
+        if (!file_ret.empty())
+        {
+            // 合成成功保存文件
+            ofile << file_ret;
+            std::cout << "语音合成成功，打开目录下的tts.mp3文件听听看" << std::endl;
+        } else {
+            // 合成出错，打印错误信息
+            std::cout << result.toStyledString();
+        }
+    }
+    */
+
+    ~SpeechRec()
+    {
+        delete client;
+        client = nullptr;
+    }
+private:
+    std::string app_id = "16618182";
+    std::string api_key = "SzXMnD2e7x5Nv86O0Szkc9sD";
+    std::string secret_key = "BV8uUFxNitDx5Hgtv7dtl3fqWirNcRPY";
+
+    aip::Speech *client;
+    std::string message;
 };
 
 // 图灵机器人类
@@ -186,13 +269,62 @@ class Jarvis{
 public:
     void Run() {
         InterRobot robot;
-        std::string s;
-        while(1) {        
-            getline(std::cin, s);
-            robot.Talk(s);
+        while(1) {    
+            message = "";    
+            getline(std::cin, message);
+            bool ret = RecordASR(message);
+            robot.Talk(message);
         }     
     }
 
+    // 执行命令
+    bool Exec(std::string command, bool print) {
+        FILE *fp = popen(command.c_str(), "r");
+        if (nullptr == fp)
+        {
+            std::cerr << "popen error!" << std::endl;
+            return false;
+        }
+        if (print)
+        {
+            char c;
+            std::cout << "-----------start-----------" << std::endl;
+            while (fread(&c, 1, 1, fp) > 0) {
+                std::cout << c;
+            }
+            std::cout << "------------end------------" << std::endl;
+        }
+        pclose(fp);
+        return true;
+    }
+
+    bool RecordASR(std::string &message) {
+        int err_code = -1;
+        std::string record = "arecord -t wav -c 1 -r 16000 -d 5 -f S16_LE";
+        record += SPEECH_FILE;
+        // 将标准输出写入null
+        record += ">/dev/null 2>$1";
+
+        std::cout << "请讲...ヾ(๑╹◡╹)ﾉ";
+        fflush(stdout);
+        if (Exec(record, false))
+        {
+            sr.ASR(err_code, message);
+            if (0 == err_code) {
+                return true;
+            }
+            std::cout << "语音识别失败...〒▽〒" << std::endl;
+        } else {
+            std::cout << "录制失败...〒▽〒" << std::endl;
+        }
+        return false;
+    }
+
+private:
+    SpeechRec sr;
+    InterRobot robot;
+    std::unordered_map<std::string, std::string> command_set;
+    std::string message;
 };
 
 #endif
